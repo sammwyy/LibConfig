76592b4399dab7ca4aae4320c8e7f3af25219423
Sammwy <sammwy.dev@gmail.com>
2023-04-05 04:29:18 -0300

    fix: removed plugin reference

--- src/main/java/com/sammwy/libconfig/plugin/LCBukkitPlugin.java
+++
@@ -1,9 +0,0 @@
-package com.sammwy.libconfig.plugin;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class LCBukkitPlugin extends JavaPlugin {
-    @Override
-    public void onEnable() {
-    }
-}
